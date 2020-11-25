a1a2822294e058735abde50d4b9b0713878e3e1b
TobiasDemoor <tobias.demoor@hotmail.com>
2020-11-25 18:47:21 -0300

    refactor distribucion config

--- config/default.js
+++ config/default.js
@@ -1,8 +1,13 @@
 "use strict";
+const path = require('path');
+const rootDir = './testFiles'
+const datadir = path.join(rootDir, 'logs');
+const commsDir = path.join(rootDir, 'comms');
 
 module.exports = {
     express: {
         port: process.env.PORT || 5000,
+        'client': path.resolve('client/build'),
     },
     jwt: {
         "TOKEN_SECRET": process.env.TOKEN_SECRET || "77aa1f16ed3d211d6ca12625",
@@ -10,17 +15,7 @@ module.exports = {
     },
     auth: {
         default: "hydroponics",
-    },
-    data: {
-        separador: ',',
-        cantRecientes: 100,
-        types: {
-            actuator: "actuator"
-        },
-        actuators: {
-            on: "on",
-            off: "off"
-        }
+        'routeUser': path.resolve('./testFiles/user.json'),
     },
     strings: {
         // auth
@@ -35,5 +30,48 @@ module.exports = {
 
         //control
         timeoutErrorMsg: 'No response from control program',
-    }
+    },
+    data: {
+        separador: ',',
+        cantRecientes: 100,
+        types: {
+            actuator: "actuator"
+        },
+        actuators: {
+            on: "on",
+            off: "off"
+        },
+        log: id => path.join(datadir, `${id}.log`),
+        columns: id => path.join(datadir, `${id}.json`),
+        sections: {
+            ambient: {
+                title: 'Ambient',
+                id: 'ambient',
+            },
+            fishtank: {
+                title: 'Fish tank',
+                id: 'fishtank',
+            },
+            upperbed: {
+                title: 'Clay media grow bed',
+                id: 'upperbed',
+            },
+            mediumbed: {
+                title: 'First deep water grow bed',
+                id: 'mediumbed',
+            },
+            lowerbed: {
+                title: 'Second deep water grow bed',
+                id: 'lowerbed',
+            },
+            general: {
+                id: 'general',
+                log: path.resolve('./testFiles/general/general.state'),
+                columns: path.resolve('./testFiles/general/general.json'),
+            }
+        },
+    },
+    comunication: {
+        path: commsDir
+    },
 }
--- config/test.js
+++ config/test.js
@@ -1,50 +1,13 @@
 "use strict";
 const path = require('path');
-const datadir = "./testFiles/logs/";
 
 module.exports = {
     jwt: {
-        "TOKEN_SECRET": "test",
-        "expTime": [2, "m"]
-    },
-    express: {
-        "client": path.resolve("client/build"),
+        TOKEN_SECRET: "test",
+        expTime: [2, "m"]
     },
     auth: {
-        "routeUser": path.resolve("./testFiles/user.test.json"),
-        "default": "test"
-    },
-    data: {
-        log: id => path.join(datadir, `${id}.log`),
-        columns: id => path.join(datadir, `${id}.test.json`),
-        sections: {
-            ambient: {
-                title: "Ambient",
-                id: "ambient",
-            },
-            fishtank: {
-                title: "Fish tank",
-                id: "fishtank",
-            },
-            upperbed: {
-                title: "Clay media grow bed",
-                id: "upperbed",
-            },
-            mediumbed: {
-                title: "First deep water grow bed",
-                id: "mediumbed",
-            },
-            lowerbed: {
-                title: "Second deep water grow bed",
-                id: "lowerbed",
-            },
-            general: {
-                log: path.resolve("./testFiles/general/general.state"),
-                columns: path.resolve("./testFiles/general/general.json"),
-            }
-        },
-    },
-    comunication: {
-        path: path.resolve("./testFiles/interaccion/")
-    },
+        routeUser: path.resolve("./testFiles/user.test.json"),
+        default: "test"
+    }
 }
